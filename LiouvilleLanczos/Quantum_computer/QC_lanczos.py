cb91624be59df565f52388ff669fea7ff90e648b
OIivier <olinahman@gmail.com>
2024-09-24 22:53:21 -0400

    Ajout de smart_inner_product_spo
    nouveau inner product gardant en memoire les chaines de Pauli déjà évaluées

--- LiouvilleLanczos/Quantum_computer/QC_lanczos.py
+++ LiouvilleLanczos/Quantum_computer/QC_lanczos.py
@@ -18,26 +18,36 @@
 
 
 """
-Componants for the Liouvillian recursion method on a Quantum computer using Qiskit.
+Components for the Liouvillian recursion method on a Quantum computer using Qiskit.
 The lanczos algorithm implementation meant to use these components is located in Lanczos.py
 """
 
-
+#%%
 from qiskit_nature.second_q import operators as op
 from qiskit_nature.second_q.operators import commutators
 from qiskit import QuantumCircuit
-from qiskit.primitives import BaseEstimatorV2
+from qiskit.primitives import BaseEstimatorV2, BaseSamplerV2
 from qiskit_nature.second_q.mappers import QubitMapper
 import numpy as np
 from typing import Optional
 
 from qiskit.transpiler import PassManager
 
+from pauliarray import WeightedPauliArray, PauliArray
+from pauliarray.partition.commutating_paulis.exclusive_fct import (  # partition_same_z,
+    partition_general_commutating,
+    partition_same_x,
+    partition_same_x_plus_special,
+)
+from pauliarray.diagonalisation.commutating_paulis.with_circuits import (
+    general_to_diagonal as general_to_diagonal_with_circuit,
+)
+
 from qiskit.quantum_info import SparsePauliOp
 
 from ..Lanczos_components import Inner_product as Base_inner_product,Summation as Base_summation
 from ..Lanczos_components import Liouvillian as BaseLiouvillian
-
+#%%
 def relative_simplify_slo(ope:op.SparseLabelOp,eps:float):
     v = max(ope.items(),key = lambda x: np.abs(x[1]))[1]
     if isinstance(ope, op.FermionicOp):
@@ -45,7 +55,7 @@ def relative_simplify_slo(ope:op.SparseLabelOp,eps:float):
     return ope.simplify(eps*v)
 
 def relative_simplify_spo(ope:SparsePauliOp,eps:float):
-    return ope.simplify(atol=1e-17,rtol=eps)
+    return ope.simplify(atol=1e-17)
 
 def separate_imag(op: SparsePauliOp):
     coeffs_real = []
@@ -57,8 +67,20 @@ def separate_imag(op: SparsePauliOp):
     pauli_op_imag = SparsePauliOp(op.paulis, coeffs_img).chop()
     return pauli_op_real, pauli_op_imag
 
-
-
+def evaluate_diag_expval(diag_obs: PauliArray, samples: dict[str: float]):
+    #evaluate expval of diagonal pauli string on dictionary of samples
+    expval = 0
+    z_string = diag_obs.z_strings[0]
+    x_string = diag_obs.x_strings[0]
+    assert np.array_equal(x_string, [False]*diag_obs.num_qubits), 'Observable is not diagonal'
+    for basis_state in samples:
+        sign = 1
+        for i in range(len(basis_state)):
+            if z_string[-1-i] and basis_state[i] == '1':
+                sign *= -1
+        expval += sign * samples[basis_state]
+    return expval
+        
 
 class inner_product_spo(Base_inner_product):
     def __init__(self, state: QuantumCircuit, estimator: BaseEstimatorV2, epsilon: int):
@@ -95,6 +117,59 @@ class inner_product_spo(Base_inner_product):
         except:
             ...
         return out
+
+class smart_inner_product_spo(Base_inner_product):
+    def __init__(self, state: QuantumCircuit, sampler: BaseSamplerV2, epsilon: int, exp_dict = {}):
+        self.state = state
+        self.sampler = sampler
+        self.eps = epsilon
+        self.exp_dict = exp_dict
+    
+    def __call__(self,A:SparsePauliOp,B:SparsePauliOp,real_result:bool=False,Name:Optional[str]=None):
+        Bc = B.adjoint()
+        f = A@Bc+Bc@A
+        f = relative_simplify_spo(f,self.eps)
+        #obs_real, obs_imag = separate_imag(f)
+        #check dictionary for previously calculated expectation values
+        f_expval = 0
+        labels_to_eval = []
+        coeffs_to_eval = []
+        for pauli in f:
+            isa_pauli = pauli.apply_layout(self.state.layout)
+            coeff = pauli.coeffs[0]
+            pauli_string = str(isa_pauli.paulis[0])
+            if pauli_string in self.exp_dict:
+                str_expval = self.exp_dict[pauli_string]
+                f_expval += str_expval * coeff
+            else:
+                labels_to_eval.append(pauli_string)
+                coeffs_to_eval.append(coeff)
+
+        try: #Add name to the list of tag for this job.
+            if Name is not None:
+                tags = self.sampler.options.environment.job_tags
+                tags.append(Name)
+                self.sampler.options.update(job_tags = tags)
+        except:
+            ...
+        
+        if len(labels_to_eval) != 0:
+            to_eval = WeightedPauliArray.from_labels_and_weights(labels_to_eval, coeffs_to_eval)
+            #group paulis into commuting cliques
+            cliques = to_eval.partition_with_fct(partition_same_x_plus_special)
+            #produce measurement circuits associated with each clique
+            for clique in cliques:
+                diag_part, factors_part, transformations_part_circuits = general_to_diagonal_with_circuit(clique.paulis)
+                qc = self.state.compose(transformations_part_circuits)
+                qc.measure_all()
+                samples = self.sampler.run(qc).result().quasi_dists[0].binary_probabilities()
+                #reconstruct expvals from samples, add to dictionary
+                for i, obs in enumerate(clique.paulis):
+                    expval = evaluate_diag_expval(diag_part[i], samples)
+                    self.exp_dict[obs.to_labels()[0]] = expval
+                    f_expval += factors_part[i] * expval
+        return f_expval
+
 
 class Liouvillian_spo(BaseLiouvillian):
 
---
+++ tests/2siteHub_smart_spo.py
@@ -0,0 +1,168 @@
+#%%
+from LiouvilleLanczos.Quantum_computer.QC_lanczos import Liouvillian_spo,smart_inner_product_spo,sum_spo
+from LiouvilleLanczos.Quantum_computer.Hamiltonian import Line_Hubbard,BoundaryCondition
+from LiouvilleLanczos.Lanczos import Lanczos
+from LiouvilleLanczos.matrix_impl import MatrixState_inner_product,Matrix_Liouvillian,Matrix_sum
+from LiouvilleLanczos.Quantum_computer.Mapping import find_best_layout
+from LiouvilleLanczos.Green import CF_Green,Green_matrix,Lehmann_Green,PolyCF_Green,PolyLehmann_Green
+from qiskit.primitives import StatevectorEstimator as pEstimator
+from qiskit.primitives import StatevectorSampler
+
+from qiskit_nature.second_q.mappers import JordanWignerMapper
+from qiskit_nature.second_q.operators import FermionicOp
+from qiskit.quantum_info.operators import SparsePauliOp
+from qiskit import QuantumCircuit
+import numpy as np
+from qiskit import transpile
+from datetime import datetime
+from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
+
+#%% problem hamiltonian and other operators setup.
+U = 4
+mapper = JordanWignerMapper()
+Ham = Line_Hubbard(-1,U/2,U,2,boundary_condition=BoundaryCondition.OPEN)
+#up spin site 1
+C2 = FermionicOp(
+    {
+        "+_2": 1,
+    },
+    num_spin_orbitals=4,
+)
+#up spin site 0
+C0 = FermionicOp(
+    {
+        "+_0": 1,
+    },
+    num_spin_orbitals=4,
+)
+C0_spo = mapper.map(C0)
+C2_spo = mapper.map(C2)
+C0_mat = C0_spo.to_matrix()
+C2_mat = C2_spo.to_matrix()
+Ham_spo = mapper.map(Ham)
+#%% Ground state circuit, obtained by inspection of analytical wavefunction
+bt = 0.7854074074074073
+GS_analytical = QuantumCircuit(4)
+GS_analytical.h(0)
+GS_analytical.x(1)
+GS_analytical.cx(0,1)
+GS_analytical.ry(bt,2)
+GS_analytical.x(3)
+GS_analytical.cx(2,3)
+GS_analytical.cx(1,3)
+GS_analytical.cx(1,2)
+GS_analytical.cz(1,2)
+GS_analytical.swap(1,2)
+
+#%% Sanity check: compare matrix ground energy with simulated estimator ground energy.
+Hmat = Ham_spo.to_matrix()
+estimator = pEstimator()
+HHam = Ham_spo
+E,S = np.linalg.eigh(Hmat)
+GS_mat = S[:,0]
+print(E[0])
+print(estimator.run([(GS_analytical,HHam)]).result()[0].data.evs)
+#%% classical computation of the Green's function at site 0
+matrix_lanczos = Lanczos(MatrixState_inner_product(GS_mat),Matrix_Liouvillian(),Matrix_sum())
+a_ed,b_ed,mu_ed = matrix_lanczos.polynomial_hybrid(Hmat,C0_mat,[C2_mat],10)
+green_ed = CF_Green(a_ed,b_ed)
+# %% Quantum computer simulation
+eps = 1e-6
+sampler = StatevectorSampler()
+SQ_inpro = smart_inner_product_spo(GS_analytical,sampler,eps)
+SQ_Liou = Liouvillian_spo(eps)
+lanczos = Lanczos(SQ_inpro,SQ_Liou,sum_spo(eps))
+a_sim5,b_sim5,mu_sim5 = lanczos.polynomial_hybrid(HHam,C0_spo,[C2_spo],10,5e-3)
+#green_sim = CF_Green(a_sim5,b_sim5)
+#%% We observe that the result are coherent.
+# import matplotlib.pyplot as plt
+# w = np.linspace(-5.5,5.5,1000)-1e-1j
+# plt.plot(w,np.imag(green_sim(w)))
+# # plt.savefig("hubu4mu2.pdf")
+# plt.plot(w,np.imag(green_ed(w)))
+# #%%
+from qiskit_ibm_runtime import (
+    Session,
+    Sampler,
+    QiskitRuntimeService,
+    Options,
+    Estimator,
+    EstimatorV2
+)
+from qiskit_ibm_runtime.options import (
+    ExecutionOptions,
+    EnvironmentOptions,
+    ResilienceOptions,
+    TranspilationOptions,
+    EstimatorOptions
+    
+)
+
+#<First time only>
+#QiskitRuntimeService.save_account(channel="ibm_quantum", token="IBM_TOKEN")
+#<\First time only>
+#If you want to reproduce my result you will have to modify this to your own provider, 
+# you may not have access to the same hardware, it might even not be in service anymore.
+service_algolab = QiskitRuntimeService(
+    channel="ibm_quantum",
+    instance = "pinq-quebec-hub/iq-quantum/algolab"
+)
+#%%
+#Sher = service_algolab.backend("ibm_sherbrooke")
+Queb =  service_algolab.backend("ibm_quebec")
+# kolk =  service_algolab.backend("ibmq_kolkata")
+# hanoi =  service_algolab.backend("ibm_hanoi")
+# auck =  service_algolab.backend("ibm_auckland")
+#backends = {"ibm_sherbrooke":Sher,"ibm_quebec":Queb}
+backends = {'ibm_quebec':Queb}
+
+class logger:
+    def __init__(self):
+        self.operator_sizes = []
+    def __call__(self,iteration,recursion_operator:SparsePauliOp,a_i,b_i):
+        self.operator_sizes.append(len(recursion_operator))
+log= logger()
+#%%
+#resilience=3 (PEC) doesn't work with ibm_sherbrooke because its ECR based instead of CNOT, and it's a significantly better quantum computer than the other options.
+#resilience=2 (ZNE) implentation is very crappy: There's no readout error mitigation leading to typically worst result than resilience=1.That's because readout error is considerable and mostly independent of circuit details. ZNE can do little to nothing about that error source.
+#resilience=1 is only readout error mitigation.
+bkd = Queb.name
+now = datetime.now()
+with Session(backend=backends[bkd]) as session:
+    estim_options = EstimatorOptions()
+    backend = backends[session.backend()]
+    estim_options.resilience_level = 1
+    estim_options.default_shots=10000 #shot noise 10000 -> ~0.01, 100000 -> ~0.003
+    estim_options.environment.job_tags = []
+    estim_options.dynamical_decoupling.enable = True
+    estim_options.dynamical_decoupling.sequence_type = 'XX'
+    estim = EstimatorV2(backend = backend, session=session,options=estim_options)
+    init_layout,GS_opt = find_best_layout(GS_analytical,backend,10,seed = 50)
+    circuit = GS_analytical.copy()
+    eps = 1e-5
+    pm = generate_preset_pass_manager(backend=backend, optimization_level=3)
+    isa_circuit = pm.run(circuit)
+    isa_obs = HHam.apply_layout(isa_circuit.layout)
+    Ground_state_E_job = estim.run([(isa_circuit, isa_obs)])
+    produit_interne = inner_product_slo(isa_circuit, estim, qubit_converter, eps)
+    Liou = Liouvillian_slo(eps)
+    Summation = sum_slo(eps)
+    lanczos = Lanczos(produit_interne,Liou,Summation,logger=log)
+    a,b,mu = lanczos.polynomial_hybrid(Ham,C0,[C2],10,5e-2)
+    E = Ground_state_E_job.result()[0].data.evs
+now = datetime.now()
+hour=now.hour
+min =now.minute
+sec = now.second
+with open(f"V2_QC_hubbard2site_data_{bkd}_{hour}_{min}_{sec}.py",'w') as txt_file:
+    txt_file.write(f"#executed on {now} \n")
+    txt_file.write(f"#resilience level: {estim_options.resilience_level} \n")
+    txt_file.write(f"#Dynamical decoupling: {estim_options.dynamical_decoupling.enable}, sequence: {estim_options.dynamical_decoupling.sequence_type} \n")    
+    txt_file.write(f"#total execution time, with wait :{datetime.now()-now}\n")
+    txt_file.write(f"#GS energy result = {E}\n")
+    txt_file.write(f'"""\n {estim.options} \n"""\n')
+    txt_file.writelines([f"a = {a}\n",f"b={b}\n",f"mu={mu}\n"])
+    txt_file.writelines([f"operator_sizes={log.operator_sizes}\n"])
+
+
+# %%
